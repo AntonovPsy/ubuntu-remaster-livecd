9b155e43519e6da290badf4747fca0582bd6ad98
AntonovPsy <88352607+AntonovPsy@users.noreply.github.com>
2021-08-03 02:06:18 +0300

    Add files via upload

---
+++ scripts/settings32.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ISO_DIR="$LIVE_DIR/i386"
+ISO='lubuntu-16.04.6-desktop-i386.iso'
+ISO_NAME='Lubuntu-16.04-i386-remaster.iso'
+
+ROOT_DIR="$LIVE_DIR/root32"
+
---
+++ scripts/settings64.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ISO_DIR="$LIVE_DIR/amd64"
+ISO='lubuntu-16.04.6-desktop-amd64.iso'
+ISO_NAME='Lubuntu-16.04-amd64-remaster.iso'
+
+ROOT_DIR="$LIVE_DIR/root64"
